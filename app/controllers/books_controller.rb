ff58780b0b076042eee224176e3425dc9d92097a
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-04-16 07:33:13 +0000

    [totyuu]theme3_bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,23 @@ class BooksController < ApplicationController
 
   def show
   end
+  
 
   def index
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+  
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   
+  #正しく実行されないため、アドレス取得時、ログアウトのアクション実行
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   get 'homes/about' => 'homes#about', as: 'about'
   
-  resources :books, only: [:new, :index, :show]
-  resources :users, only: [:show, :edit]
+  resources :books, only: [:new, :index, :show, :create]
+  resources :users, only: [:show, :edit, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
